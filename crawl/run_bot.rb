028caec65b043fdef2acc4ccb9da1824c82bb90d
Adam K Phillipps <adam.phillipps@wgu.edu>
2018-09-23 17:48:47 -0600

    Still working on it

--- crawl/run_bot.rb
+++ crawl/run_bot.rb
@@ -1,19 +1,21 @@
 require_relative './test_helper.rb'
 require 'logger'
 
-logger = Logger.new(STDOUT)
-logger.level = ENV['LOGLEVEL'] || Logger::DEBUG
+logger = Logger.new(ENV['LOG_PATH'] || STDOUT)
+logger.level = ENV['LOG_LEVEL'] || Logger::DEBUG
 
 # stuff as seen
 class WTFE
   include Capybara::DSL
   def initialize(domain = ENV['HOST'], queue_url = ENV['QUEUE_URL'])
-    logger.debug("domain: #{domain}, queue_url: #{queue_url}")
-    visit domain
-    File.open("out.txt", 'w') { |f| f.write("write your stuff here #{Time.now}") }
-
     sleep 20
+    logger.debug("domain: #{domain}, queue_url: #{queue_url}")
+    puts "domain: #{domain}, queue_url: #{queue_url}"
+    visit domain
+    File.open("out.txt", 'w') do |f|
+      f.write("write your stuff here #{Time.now}")
+    end
   end
 end
 
-WTFE.new(ARGV.first)
+WTFE.new(*ARGV[1, -1])
